8fbfca9c8b85f4b4801988d08aaf4be0a0c14d82
ConstMatador <zhongzheng.wang.private@gmail.com>
2026-04-15 17:39:39 +0000

    222

--- generate_poster_multiGPU.bash
+++ generate_poster_multiGPU.bash
@@ -1,18 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 generate_poster_multiGPU.py \
-  --train_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_train_subset_with_farthest.csv \
-  --val_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_val_subset_with_farthest.csv \
-  --test_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_test_subset_with_farthest.csv \
-  --summary_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/summary_subset.csv \
-  --poster_dir /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/MoviePosters \
-  --output_dir save/PosterGenerator \
-  --test_save_dir TestPosters \
-  --epochs 10 \
-  --train_batch_size 50 \
-  --val_batch_size 25 \
-  --test_batch_size 25 \
-  --max_summary_words 50
-
-# nohup bash -c '
 # CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 generate_poster_multiGPU.py \
 #   --train_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_train_subset_with_farthest.csv \
 #   --val_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_val_subset_with_farthest.csv \
@@ -22,8 +7,23 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 generate_poster_multiGP
 #   --output_dir save/PosterGenerator \
 #   --test_save_dir TestPosters \
 #   --epochs 10 \
-#   --train_batch_size 50 \
+#   --train_batch_size 25 \
 #   --val_batch_size 25 \
 #   --test_batch_size 25 \
 #   --max_summary_words 50
-# ' > log/train.log 2>&1 &
+
+nohup bash -c '
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 generate_poster_multiGPU.py \
+  --train_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_train_subset_with_farthest.csv \
+  --val_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_val_subset_with_farthest.csv \
+  --test_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/UserEmbeddings/user_embeddings_test_subset_with_farthest.csv \
+  --summary_csv /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/summary_subset.csv \
+  --poster_dir /root/TOS/ZhongzhengWang/dataset/MovieLensLatest/MoviePosters \
+  --output_dir save/PosterGenerator \
+  --test_save_dir TestPosters \
+  --epochs 10 \
+  --train_batch_size 25 \
+  --val_batch_size 25 \
+  --test_batch_size 25 \
+  --max_summary_words 50
+' > log/train.log 2>&1 &
